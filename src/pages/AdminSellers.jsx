4e85b269b058cdff982426e6eb7bc6915767c661
CNHPARA TODOS <tcnhpara@gmail.com>
2026-01-30 00:55:46 +0000

    File changes

--- src/pages/AdminSellers.jsx
+++ src/pages/AdminSellers.jsx
@@ -9,7 +9,8 @@ import {
   Save,
   Phone,
   Mail,
-  ArrowLeft
+  ArrowLeft,
+  Lock
 } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
