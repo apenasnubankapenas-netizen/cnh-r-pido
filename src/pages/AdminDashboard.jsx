72dd2318b25f397719f3227de8e4f71522e9e1e5
CNHPARA TODOS <tcnhpara@gmail.com>
2026-02-06 14:20:16 +0000

    Reordenado SetInstructor, Ajustado Dependências

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -347,7 +347,6 @@ export default function AdminDashboard() {
           </Card>
         </Link>
 
-        {/* BLOQUEIO: Link de pagamentos oculto para instrutores */}
         {!isInstructor && (
           <Link to={createPageUrl('AdminPayments')}>
             <Card className="bg-[#1a2332] border-[#374151] hover:border-green-500 transition-all cursor-pointer">
